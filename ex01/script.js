cb41b4f6d00c149a18583152836d78e37e62b2ed
patrick <patrick.zamblebi@yahoo.fr>
2020-03-13 12:48:52 +0100

    Push_ex01

---
+++ ex01/script.js
@@ -0,0 +1,20 @@
+var tutu = "Patrick";
+
+
+console.log(tutu);
+
+var num = 4;
+
+console.log(num);
+
+var x = false;
+var y = true; 
+console.log(x);
+console.log(y);
+
+var object = {
+    name: "Patrick", 
+    age: 67
+}
+
+console.log(object)
