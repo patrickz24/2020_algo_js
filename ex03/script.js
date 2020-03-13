efd624c92fcf4ba06a11451c5a5b98a483e168ea
patrick <patrick.zamblebi@yahoo.fr>
2020-03-13 14:23:24 +0100

    ex03

---
+++ ex03/script.js
@@ -0,0 +1,23 @@
+var a = 8;
+var b = a+=1;
+console.log(b);
+var a = a + 1;
+console.log(a);
+var a = a*2;
+console.log(a);
+var b = b - 5;
+ console.log(b);
+ var b = b*b;
+ console.log(b);
+var a = a + 3;
+console.log(a);
+
+if(a==1){ var a = a + 2;
+    console.log(a);
+}
+else {
+    var a = a +3;
+    console.log(a);
+}
+
+
