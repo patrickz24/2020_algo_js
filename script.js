baa39f8c66925d6576486e448e10cf814ac7521f
patrick <patrick.zamblebi@yahoo.fr>
2020-03-13 11:56:03 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var tutu = "Patrick";
+
+
+console.log(tutu);
+
+var num = 4;
+
+console.log(num);
+
+var arr = new Array("Pat", "sam", "luc");
+var arr = Array("chien", "chat", "tigre");
+var arr = [8, 9, 20];
